1100811b6e54df25218e3a513ef5d00c66e2b76f
Manuel <mretana@nevada.unr.edu>
2020-01-31 22:13:03 -0800

    created new file for unity object selection

---
+++ Assets/SelectionManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+    
+    private void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+
+                _selection = selection;
+            }
+        }
+    }
+}
